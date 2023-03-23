aba98e2d30461cb4426f8bcca66c06a4d888ea09
MIGUEL ÁNGEL ARIAS SÁNCHEZ <miguelariasdev@gmail.com>
2023-03-23 17:45:46 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SearchComponent,
-    ArtistaComponent
+    ArtistaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
